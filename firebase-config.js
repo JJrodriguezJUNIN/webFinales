33eba5aec348b32f8b4cef39eda9cc5b1072f20a
JJrodriguezJUNIN <152146042+JJrodriguezJUNIN@users.noreply.github.com>
2025-01-02 13:17:15 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+ apiKey: "AIzaSyAJDW1lZuh4swoKG4FZkPjvCMRUnvvPHL0",
+  authDomain: "appfinales.firebaseapp.com",
+  projectId: "appfinales",
+  storageBucket: "appfinales.firebasestorage.app",
+  messagingSenderId: "55034439618",
+  appId: "1:55034439618:web:5103e4b70a536583dd585b"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore(app);
+
+//exportar db para usar en otros archivos
+export { db };

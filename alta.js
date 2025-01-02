c1c38197973d02e4df60d0924f92b82c5dfd3e14
JJrodriguezJUNIN <152146042+JJrodriguezJUNIN@users.noreply.github.com>
2025-01-02 13:00:03 -0300

    Update alta.js

--- alta.js
+++ alta.js
@@ -1,13 +1,12 @@
 // Inicializar Firebase (CON TU CONFIGURACIÓN)
 const firebaseConfig = {
-    apiKey: "AIzaSyAJDW1lZuh4swoKG4FZkPjvCMRUnvvPHL0",
-    authDomain: "appfinales.firebaseapp.com",
-    projectId: "appfinales",
-    storageBucket: "appfinales.firebasestorage.app",
-    messagingSenderId: "55034439618",
-    appId: "1:55034439618:web:5103e4b70a536583dd585b"
+  apiKey: "AIzaSyAJDW1lZuh4swoKG4FZkPjvCMRUnvvPHL0",
+  authDomain: "appfinales.firebaseapp.com",
+  projectId: "appfinales",
+  storageBucket: "appfinales.firebasestorage.app",
+  messagingSenderId: "55034439618",
+  appId: "1:55034439618:web:5103e4b70a536583dd585b"
 };
-
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore(app);

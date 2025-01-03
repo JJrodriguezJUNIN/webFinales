1de350f5e1bf53b8a6112b1192ec1a986983dedb
JJrodriguezJUNIN <152146042+JJrodriguezJUNIN@users.noreply.github.com>
2025-01-03 09:50:39 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,10 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Tu configuración de Firebase
 const firebaseConfig = {
- apiKey: "AIzaSyAJDW1lZuh4swoKG4FZkPjvCMRUnvvPHL0",
+  apiKey: "AIzaSyAJDW1lZuh4swoKG4FZkPjvCMRUnvvPHL0",
   authDomain: "appfinales.firebaseapp.com",
   projectId: "appfinales",
   storageBucket: "appfinales.firebasestorage.app",
@@ -7,9 +12,14 @@ const firebaseConfig = {
   appId: "1:55034439618:web:5103e4b70a536583dd585b"
 };
 
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore(app);
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
 
-//exportar db para usar en otros archivos
-export { db };
-console.log("firebase-config")
+// Obtiene las instancias de autenticación y firestore
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Exporta auth y db para su uso en otros archivos
+export { auth, db };
+
+console.log("firebase-config.js cargado"); // Mensaje informativo
